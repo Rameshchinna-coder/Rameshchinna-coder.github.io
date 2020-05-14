825d5b51ec323b98e8c8544620f8e462285c9b52
Ramesh <ramiga143@gmail.com>
2020-05-15 01:51:54 +0530

    Updates

---
+++ precache-manifest.b160580b0c7a0ad882fed818aad024e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94f5002d048fb54dc2df",
+    "url": "/Rameshchinna-coder.github.io/static/css/main.6811fb35.chunk.css"
+  },
+  {
+    "revision": "94f5002d048fb54dc2df",
+    "url": "/Rameshchinna-coder.github.io/static/js/main.94f5002d.chunk.js"
+  },
+  {
+    "revision": "091c9b34dfb7da79347a",
+    "url": "/Rameshchinna-coder.github.io/static/js/runtime~main.091c9b34.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/Rameshchinna-coder.github.io/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "33cd6fbefb2d2f65ad11d728d1a1a911",
+    "url": "/Rameshchinna-coder.github.io/index.html"
+  }
+];
